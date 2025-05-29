57199243d46643407494a16ceb17bc48b812f397
Muhtasham Nawaz <muhtasham.nawaz@propsure.com.pk>
2025-05-29 15:16:36 +0500

    cleaned some code and fixed a state issue

--- app/(app)/chat/footer.tsx
+++ app/(app)/chat/footer.tsx
@@ -1,72 +1,27 @@
-import React, { useRef, useState } from "react"
-import { View, TouchableOpacity, TextInput, Animated, Pressable } from "react-native"
+import React, { useState } from "react"
+import { View, TextInput, Pressable } from "react-native"
 import Send from "@/components/icons/Send";
-// import Mic from "@/components/icons/Mic";
-// import Attach from "@/components/icons/Attach";
-// import Emoji from "@/components/icons/Emoji";
 
 export default (props: any) => {
     const {
         styles,
-        inputText,
-        setInputText,
         sendMessage
     } = props;
 
-    // const [isRecording, setIsRecording] = useState(false);
-
-    // const animatedValue = useRef(new Animated.Value(0)).current;
-
-    // Start recording animation
-    
-    // const startRecording = () => {
-    //     setIsRecording(true);
-    //     Animated.timing(animatedValue, {
-    //     toValue: 1,
-    //     duration: 300,
-    //     useNativeDriver: true,
-    //     }).start();
-    // };
-
-    // Stop recording animation
-    
-    // const stopRecording = () => {
-    //     setIsRecording(false);
-    //     Animated.timing(animatedValue, {
-    //     toValue: 0,
-    //     duration: 300,
-    //     useNativeDriver: true,
-    //     }).start();
-    // };
+    const [inputText, setInputText] = useState('');
 
     return (
         <View style={styles.footer}>
-            {/* <TouchableOpacity>
-                <Emoji name="happy-outline" size={25} color="#fefdfe" />
-            </TouchableOpacity> */}
             <TextInput
                 style={styles.textInput}
                 placeholder="Type a message"
                 placeholderTextColor={"#AAA"}
                 value={inputText}
-                onChangeText={setInputText}
+                onChangeText={(text) => setInputText(text)}
             />
-            <Pressable hitSlop={10} style={styles.sendButton} onPress={sendMessage}>
+            <Pressable hitSlop={10} style={styles.sendButton} onPress={() => sendMessage(inputText, setInputText)}>
                 <Send name="send" size={25} color="#4caf50" />
             </Pressable>
-            {/* {inputText ? (
-                <TouchableOpacity onPress={sendMessage}>
-                    <Send name="send" size={25} color="#4caf50" />
-                </TouchableOpacity>
-            ) : (
-                <TouchableOpacity
-                    onLongPress={startRecording}
-                    onPressOut={stopRecording}
-                    style={styles.micIcon}
-                >
-                    <Mic name="mic" size={25} color={isRecording ? 'red' : '#fefdfe'} />
-                </TouchableOpacity>
-            )} */}
         </View>
     )
 }
--- app/(app)/chat/index.tsx
+++ app/(app)/chat/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, ActivityIndicator} from 'react-native';
 import Modal from 'react-native-modal';
 import Body from './body';
 import Footer from './footer';
@@ -9,36 +9,11 @@ import styles from './styles';
 
 const ChatScreen = () => {
 
-  const [inputText, setInputText] = useState('');
-  // const [messages, setMessages] = useState<any>([
-  //   // Text Messages
-  //   { id: Date.now(), type: 'text', content: 'Hello, how are you today?', sender: 'me' },
-  //   { id: Date.now() + 1, type: 'text', content: 'Let’s discuss the project details later.', sender: 'me' },
-  //   { id: Date.now() + 2, type: 'text', content: 'Please review the document attached below.', sender: 'you' },
-  //   { id: Date.now() + 3, type: 'text', content: 'Hello, how are you today?', sender: 'me' },
-  //   { id: Date.now() + 4, type: 'text', content: 'Let’s discuss the project details later.', sender: 'me' },
-  //   { id: Date.now() + 5, type: 'text', content: 'Please review the document attached below.', sender: 'you' },
-  //   { id: Date.now() + 6, type: 'text', content: 'Hello, how are you today?', sender: 'me' },
-  //   { id: Date.now() + 7, type: 'text', content: 'Let’s discuss the project details later.', sender: 'me' },
-  //   { id: Date.now() + 8, type: 'text', content: 'Please review the document attached below.', sender: 'you' },
-  //   { id: Date.now() + 9, type: 'text', content: 'Hello, how are you today?', sender: 'me' },
-  //   { id: Date.now() + 10, type: 'text', content: 'Let’s discuss the project details later.', sender: 'me' },
-  //   { id: Date.now() + 11, type: 'text', content: 'Please review the document attached below.', sender: 'you' },
-  // ].reverse());
-
   const { messages, initializing, sendMessage } = useDeepSeek();
 
   const [showMessageMenu, setShowMessageMenu] = useState(false);
 
-  // Function to add new message to chat
-  // const sendMessage = () => {
-  //   if (inputText.trim()) {
-  //     setMessages((prev: any[]) => [...(prev.reverse()), { id: Date.now(), type:'text', content: inputText, sender: 'me' }].reverse());
-  //     setInputText('');
-  //   }
-  // };
-
-  const onSend = async () => {
+  const onSend = async (inputText: string, setInputText: (text: string) => void) => {
     if (!inputText.trim()) return;
     await sendMessage(inputText.trim());
     setInputText('');
@@ -50,6 +25,7 @@ const ChatScreen = () => {
 
   return (
     <SafeAreaView style={styles.container}>
+      {initializing && <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}><ActivityIndicator size={50} /></View>}
       {!initializing && (<View style={{flex: 1}}>
         {/* Chat Body */}
         <Body
@@ -62,8 +38,6 @@ const ChatScreen = () => {
         <Footer
           styles={styles}
           sendMessage={onSend}
-          inputText={inputText}
-          setInputText={setInputText}
         />
       </View>)}
 
